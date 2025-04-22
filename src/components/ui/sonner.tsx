20a5e2bec1f5ac4a77ef0bc61485c59a5330628f
mst-mkt <mail@keito.dev>
2025-04-22 10:16:29 +0900

    fix: remove import from next-themes

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,14 +1,14 @@
 "use client"
 
-import { useTheme } from "next-themes"
+import { useTheme } from "@/lib/theme/useTheme"
 import { Toaster as Sonner, ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme } = useTheme()
 
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme={theme}
       className="toaster group"
       style={
         {
