ca879069f395cb2d9a5b156ab35d7b777b2b3e09
1F10230048 <s1f102300481@iniad.org>
2025-04-23 01:12:38 +0900

    アニメ情報取得APIの追加

---
+++ src/app/actions/api/get-works.ts
@@ -0,0 +1,20 @@
+'use server'
+
+import { annictApiClient } from '@/lib/api/annict-rest'
+import { auth } from '@/lib/auth'
+
+export const getWorks = async (id: number) => {
+  await auth()
+
+  const workResult = await annictApiClient.getWorks({
+    query: { filter_ids: [id], per_page: 1 },
+  })
+
+  if (!workResult.isOk) {
+    console.error(`Failed to fetch work (id:${id}):`, workResult.error)
+    return null
+  }
+
+  const [work] = workResult.value.works
+  return work
+}
