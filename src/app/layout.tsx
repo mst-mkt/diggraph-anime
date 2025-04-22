9cf4a3972004e2155988c926576a1b821d62f3a7
mst-mkt <mail@keito.dev>
2025-04-22 18:42:11 +0900

    chore: add nuqs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import { BASIC_METADATA } from '@/constants/project'
 import { ThemeLoader } from '@/lib/theme/theme-loader'
 import { SessionProvider } from 'next-auth/react'
+import { NuqsAdapter } from 'nuqs/adapters/next'
 import type { FC, ReactNode } from 'react'
 
 export const metadata = BASIC_METADATA
@@ -14,7 +15,9 @@ const RootLayout: FC<RootLayoutProps> = ({ children }) => (
   <html lang="ja" suppressHydrationWarning={true}>
     <ThemeLoader />
     <body>
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProvider>
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </SessionProvider>
     </body>
   </html>
 )
