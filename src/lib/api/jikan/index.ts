498a2221d91e2e6b670b37f5f43e6771fe8c5b0c
mst-mkt <mail@keito.dev>
2025-04-26 00:09:49 +0900

    fix: enable data-cache for jikan-api-client

--- src/lib/api/jikan/index.ts
+++ src/lib/api/jikan/index.ts
@@ -4,4 +4,14 @@ import type { paths } from './schema.gen'
 
 export const jikanApiClient = createClient<paths>({
   baseUrl: JIKAN_API_BASEURL,
+  fetch: (input: RequestInfo, init: RequestInit = {}) => {
+    return fetch(input, {
+      cache: 'force-cache',
+      ...init,
+      next: {
+        revalidate: 60 * 60 * 24 * 7,
+        ...init.next,
+      },
+    })
+  },
 })
