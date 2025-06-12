302351182872c1be1e4b108030a7b353b8d86fc7
rarandeyo <rarandeyo@gmail.com>
2025-06-12 21:52:22 +0900

    fix: increase z-index for header to skeleton

--- src/app/(main)/_layout/header.tsx
+++ src/app/(main)/_layout/header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { ThemeButton } from './theme-button.client'
 
 export const Header = () => (
-  <header className="sticky top-0 border-muted border-b px-[4svw] backdrop-blur-md">
+  <header className="sticky top-0 z-30 border-muted border-b px-[4svw] backdrop-blur-md">
     <div className="mx-auto flex w-full max-w-160 items-center gap-x-4 py-3">
       <PickaxeIcon size={24} className="text-diggraph-accent" />
       <Link href="/" className="grow">
