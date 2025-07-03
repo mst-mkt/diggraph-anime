568ec5da2fec7ef4925c6204cd2da7b7804db965
mst-mkt <mail@keito.dev>
2025-07-03 15:03:28 +0900

    add: annict api client for visitors with personal access token

--- src/lib/api/annict-rest/index.ts
+++ src/lib/api/annict-rest/index.ts
@@ -1,4 +1,10 @@
+import { ANNICT_TOKEN } from '@/lib/env-variables'
 import { ANNICT_API_BASEURL } from '../../../constants/annict'
 import { AnnictClient } from './client'
+import { AnnictClientForVisitor } from './visitor-client'
 
 export const annictApiClient = new AnnictClient(`${ANNICT_API_BASEURL}/v1`)
+export const annictApiClientForVisitor = new AnnictClientForVisitor(
+  `${ANNICT_API_BASEURL}/v1`,
+  ANNICT_TOKEN,
+)
