aafab896291411b2029b8f01fb9919e1d0d0953b
mst-mkt <mail@keito.dev>
2025-07-05 05:05:40 +0900

    fix: adjust header styles

--- src/app/(app)/_layout/header.tsx
+++ src/app/(app)/_layout/header.tsx
@@ -1,15 +1,21 @@
 import { VisitorDialog } from '@/app/_layout/visitor-dialog'
-import { PROJECT_NAME } from '@/constants/project'
-import { PickaxeIcon } from 'lucide-react'
+import { cn } from '@/lib/classnames'
+import { Bonheur_Royale } from 'next/font/google'
 import Link from 'next/link'
 import { type FC, Suspense } from 'react'
 import { ThemeButton } from './theme-button.client'
 
+const bonheur = Bonheur_Royale({ weight: '400' })
+
 export const Header: FC = () => (
-  <header className="flex h-fit w-full items-center gap-x-4 border border-bottom border-muted px-[4svw] py-3 md:px-8">
-    <PickaxeIcon size={24} className="text-diggraph-accent" />
+  <header className="flex h-fit w-full items-center gap-x-4 border-muted border-b-2 px-[4svw] py-3 md:px-8">
     <Link href="/" className="grow">
-      <h1 className="font-bold text-lg">{PROJECT_NAME}</h1>
+      <h1 className="flex items-center gap-x-1 truncate font-bold text-xl">
+        <span>Diggraph</span>
+        <span className={cn('-ml-3 mt-1 text-3xl text-diggraph-accent', bonheur.className)}>
+          Anime
+        </span>
+      </h1>
     </Link>
     <Suspense>
       <VisitorDialog />
--- src/app/(main)/_layout/header.tsx
+++ src/app/(main)/_layout/header.tsx
@@ -1,16 +1,22 @@
 import { VisitorDialog } from '@/app/_layout/visitor-dialog'
-import { PROJECT_NAME } from '@/constants/project'
-import { PickaxeIcon } from 'lucide-react'
+import { cn } from '@/lib/classnames'
+import { Bonheur_Royale } from 'next/font/google'
 import Link from 'next/link'
 import { Suspense } from 'react'
 import { ThemeButton } from './theme-button.client'
 
+const bonheur = Bonheur_Royale({ weight: '400' })
+
 export const Header = () => (
-  <header className="sticky top-0 z-10 border-muted border-b px-[4svw] backdrop-blur-md">
+  <header className="sticky top-0 z-10 px-[4svw] backdrop-blur-md">
     <div className="mx-auto flex w-full max-w-160 items-center gap-x-4 py-3">
-      <PickaxeIcon size={24} className="text-diggraph-accent" />
       <Link href="/" className="grow">
-        <h1 className="truncate font-bold text-lg">{PROJECT_NAME}</h1>
+        <h1 className="flex items-center gap-x-1 truncate font-bold text-xl">
+          <span>Diggraph</span>
+          <span className={cn('-ml-3 mt-1 text-3xl text-diggraph-accent', bonheur.className)}>
+            Anime
+          </span>
+        </h1>
       </Link>
       <Suspense>
         <VisitorDialog />
